0dd072c57a8c2c70d0c4edc87a40edb9584b3f43
Chris Reynolds <chris@jazzsequence.com>
2016-06-19 10:55:34 -0600

    remove class extension of taxonomy_core, we're going to register multiple taxonomies here

--- includes/class-rb-taxonomies.php
+++ includes/class-rb-taxonomies.php
@@ -14,7 +14,7 @@ require_once dirname( __FILE__ ) . '/../vendor/taxonomy-core/Taxonomy_Core.php';
  * @see https://github.com/WebDevStudios/Taxonomy_Core
  * @since NEXT
  */
-class RB_Taxonomies extends Taxonomy_Core {
+class RB_Taxonomies {
 	/**
 	 * Parent plugin class
 	 *
@@ -25,32 +25,19 @@ class RB_Taxonomies extends Taxonomy_Core {
 
 	/**
 	 * Constructor
-	 * Register Taxonomy. See documentation in Taxonomy_Core, and in wp-includes/taxonomy.php
 	 *
 	 * @since NEXT
 	 * @param  object $plugin Main plugin object.
-	 * @return void
 	 */
 	public function __construct( $plugin ) {
 		$this->plugin = $plugin;
 		$this->hooks();
-
-		// Register this taxonomy
-		// First parameter should be an array with Singular, Plural, and Registered name
-		// Second parameter is the register taxonomy arguments
-		// Third parameter is post types to attach to.
-		parent::__construct(
-			array( __( 'Singular', 'recipe-box' ), __( 'Plural', 'recipe-box' ), 'slug' ),
-			array( 'hierarchical' => false ),
-			array( 'rb_recipe' )
-		);
 	}
 
 	/**
 	 * Initiate our hooks
 	 *
 	 * @since NEXT
-	 * @return void
 	 */
 	public function hooks() {
 	}
