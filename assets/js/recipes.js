7aaed6ff4fec1444bb71247f1394ad52e9c74ff4
Chris Reynolds <chris@jazzsequence.com>
2016-06-19 11:16:52 -0600

    adds js to replace the remove group button with a trash can icon

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -1,4 +1,48 @@
-jQuery(document).ready(function($) {
+/**
+ * Replaces the CMB2 remove group button with a trash can.
+ * @author Greg Rickaby
+ */
+window.recipe_box_remove_group_row = {};
+( function( window, $, that ) {
+
+	// Constructor.
+	that.init = function() {
+		that.cache();
+
+		if ( that.meetsRequirements ) {
+			that.bindEvents();
+		}
+	};
+
+	// Cache all the things.
+	that.cache = function() {
+		that.$c = {
+			window: $(window),
+			recipesPage: $( 'body.post-type-rb_recipe' ),
+			removeGroupRowButton: $( '.ingredients .cmb-remove-row .cmb-remove-group-row' )
+		};
+	};
+
+	// Combine all events.
+	that.bindEvents = function() {
+		that.$c.window.on( 'load', that.doTrashCan );
+	};
+
+	// Do we meet the requirements?
+	that.meetsRequirements = function() {
+		return that.$c.recipesPage.length;
+	};
+
+	// Replace HTML with a Dashicon "trash can".
+	that.doTrashCan = function() {
+		that.$c.removeGroupRowButton.html( '<span class="dashicons dashicons-trash"></span>' );
+	};
+
+	// Engage!
+	$( that.init );
+
+})( window, jQuery, window.recipe_box_remove_group_row );
+
 	// Show console log if debugging is active.
 	if ( recipes.wp_debug ) {
 		console.log( recipes.autosuggest );
