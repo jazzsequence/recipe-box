e4ccf3c5be6d885f1f4b707ecb082de81da3ec8a
Chris Reynolds <chris@jazzsequence.com>
2016-06-19 13:39:06 -0600

    integrate tgmpa to recommend the WP API

--- recipe-box.php
+++ recipe-box.php
@@ -254,6 +254,80 @@ final class Recipe_Box {
 		echo '</div>';
 	}
 
+
+	/**
+	 * Register the required plugins for this theme.
+	 *
+	 * We're recommending (but not requiring) the WP-JSON API.
+	 *
+	 * This function is hooked into tgmpa_init, which is fired within the
+	 * TGM_Plugin_Activation class constructor.
+	 *
+	 * @uses  tgmpa
+	 * @link  https://github.com/TGMPA/TGM-Plugin-Activation
+	 */
+	public function register_required_plugins() {
+		/*
+		 * Array of plugin arrays. Required keys are name and slug.
+		 * If the source is NOT from the .org repo, then source is also required.
+		 */
+		$plugins = array(
+			array(
+				'name'      => 'WordPress REST API',
+				'slug'      => 'rest-api',
+				'required'  => false,
+			),
+		);
+
+		/*
+		 * Array of configuration settings.
+		 */
+		$config = array(
+			'id'           => 'recipe-box',       // Unique ID for hashing notices for multiple instances of TGMPA.
+			'default_path' => '',                      // Default absolute path to bundled plugins.
+			'menu'         => 'tgmpa-install-plugins', // Menu slug.
+			'parent_slug'  => 'plugins.php',           // Parent menu slug.
+			'capability'   => 'manage_options',        // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+			'has_notices'  => true,                    // Show admin notices or not.
+			'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+			'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+			'is_automatic' => true,                    // Automatically activate plugins after installation or not.
+			'message'      => '',                      // Message to output right before the plugins table.
+
+			'strings'      => array(
+				'notice_can_install_required'     => _n_noop(
+					'Recipe Box requires the following plugin: %1$s.',
+					'Recipe Box requires the following plugins: %1$s.',
+					'recipe-box'
+				), // %1$s = plugin name(s).
+				'notice_can_install_recommended'  => _n_noop(
+					'Recipe Box recommends the %1$s plugin for autocompletion support for ingredients.',
+					'Recipe Box recommends the following plugins: %1$s.',
+					'recipe-box'
+				), // %1$s = plugin name(s).
+				'notice_ask_to_update'            => _n_noop(
+					'The following plugin needs to be updated to its latest version to ensure maximum compatibility with Recipe Box: %1$s.',
+					'The following plugins need to be updated to their latest version to ensure maximum compatibility with Recipe Box: %1$s.',
+					'recipe-box'
+				), // %1$s = plugin name(s).
+				'notice_can_activate_recommended' => _n_noop(
+					'The following recommended plugin is currently inactive: %1$s. This plugin is used for autocompletion support on ingredients.',
+					'The following recommended plugins are currently inactive: %1$s.',
+					'recipe-box'
+				), // %1$s = plugin name(s).
+				'activate_link'                   => _n_noop(
+					'Activate plugin',
+					'Activate plugins',
+					'recipe-box'
+				),
+				'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for Recipe Box. Please update the plugin.', 'recipe-box' ),  // %1$s = plugin name(s).
+				'nag_type'                        => 'updated', // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
+			),
+		);
+
+		tgmpa( $plugins, $config );
+	}
+
 	/**
 	 * Magic getter for our object.
 	 *
