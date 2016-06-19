7f9af3bd4f801b2f4db62c518dcc7a794a66a6f4
Chris Reynolds <chris@jazzsequence.com>
2016-06-19 11:17:16 -0600

    refactors autosuggest code to be more object oriented

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -43,27 +43,50 @@ window.recipe_box_remove_group_row = {};
 
 })( window, jQuery, window.recipe_box_remove_group_row );
 
+
+window.recipe_box_autosuggest = {};
+( function( window, $, that ) {
+
+	// Constructor.
+	that.init = function() {
+		that.cache();
+
+		if ( that.meetsRequirements ) {
+			that.bindEvents();
+		}
+	};
+
+ 	// Cache all the things.
+	that.cache = function() {
+		that.$c = {
+			window: $(window),
+			wp_debug: recipes.wp_debug,
+			autosuggest: recipes.autosuggest,
+			ingredient: '.cmb-repeatable-grouping input.ingredient',
+		};
+	};
+
 	// Show console log if debugging is active.
-	if ( recipes.wp_debug ) {
-		console.log( recipes.autosuggest );
+	if ( that.$c.wp_debug ) {
+		console.log( that.$c.autosuggest );
 	}
 
-	if ( recipes.autosuggest ) {
+	if ( that.$c.autosuggest ) {
 		// wp_localize_script will convert our array into an object. We need to convert it back to an array.
-		recipes.autosuggest = $.map( recipes.autosuggest, function( value, index ){
+		that.$c.autosuggest = $.map( that.$c.autosuggest, function( value, index ){
 			return [value];
 		});
 
 		// Log the autosuggest array after we've modified it.
-		if ( recipes.wp_debug ) {
-			console.log( recipes.autosuggest );
+		if ( that.$c.wp_debug ) {
+			console.log( that.$c.autosuggest );
 		}
 
 		// When an ingredient input is clicked, trigger the autocompletion script.
-		$('body').on('click','.cmb-repeatable-grouping input.ingredient',function() {
+		$('body').on('click',that.$c.ingredient,function() {
 			$(this).autocomplete({
-				source: recipes.autosuggest,
+				source: that.$c.autosuggest,
 			});
 		} )
 	}
-});
+})( window, jQuery, window.recipe_box_autosuggest );
