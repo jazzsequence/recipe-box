2f87beccfb270505f1646208377ed456ca114313
Chris Reynolds <chris@jazzsequence.com>
2016-06-19 10:56:08 -0600

    register recipe cat, meal type and cuisine categories

--- includes/class-rb-taxonomies.php
+++ includes/class-rb-taxonomies.php
@@ -40,5 +40,43 @@ class RB_Taxonomies {
 	 * @since NEXT
 	 */
 	public function hooks() {
+		add_action( 'init', array( $this, 'register_taxonomies' ), 4 );
+	}
+
+	/**
+	 * Register Taxonomy. See documentation in Taxonomy_Core, and in wp-includes/taxonomy.php
+	 *
+	 * @since NEXT
+	 */
+	public function register_taxonomies() {
+		// Recipe Category.
+		register_via_taxonomy_core( array(
+				__( 'Recipe Category', 'recipe-box' ),   // Singular.
+				__( 'Recipe Categorys', 'recipe-box' ),  // Plural.
+				'rb_recipe_category',                    // Registered name.
+			),
+			array(),             // Array of taxonomy arguments.
+			array( 'rb_recipe' ) // Array of post types.
+		);
+
+		// Meal Types.
+		register_via_taxonomy_core( array(
+				__( 'Meal Type', 'recipe-box' ),  // Singular.
+				__( 'Meal Types', 'recipe-box' ), // Plural.
+				'rb_meal_type',                   // Registered name.
+			),
+			array(),             // Array of taxonomy arguments.
+			array( 'rb_recipe' ) // Array of post types.
+		);
+
+		// Recipe Cuisines.
+		register_via_taxonomy_core( array(
+				__( 'Cuisine', 'recipe-box' ),  // Singular.
+				__( 'Cuisines', 'recipe-box' ), // Plural.
+				'rb_recipe_cuisine',            // Registered name.
+			),
+			array(),             // Array of taxonomy arguments.
+			array( 'rb_recipe' ) // Array of post types.
+		);
 	}
 }
