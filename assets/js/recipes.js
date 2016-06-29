8846d6184d71eeb3012d74e9516a37a9242db403
Chris Reynolds <chris@jazzsequence.com>
2016-06-29 16:38:22 -0600

    combine js into one class

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -1,36 +1,29 @@
-/**
- * Replaces the CMB2 remove group button with a trash can.
- * @author Greg Rickaby
- */
-window.recipe_box_remove_group_row = {};
+window.recipe_box = {};
 ( function( window, $, that ) {
 
 	// Constructor.
 	that.init = function() {
 		that.cache();
-
-		if ( that.meetsRequirements ) {
-			that.bindEvents();
-		}
-	};
-
-	// Cache all the things.
-	that.cache = function() {
-		that.$c = {
-			window: $(window),
-			recipesPage: $( 'body.post-type-rb_recipe' ),
-			removeGroupRowButton: $( '.ingredients .cmb-remove-row .cmb-remove-group-row' )
-		};
+		that.bindEvents();
 	};
 
 	// Combine all events.
 	that.bindEvents = function() {
 		that.$c.window.on( 'load', that.doTrashCan );
+		that.$c.window.on( 'load', that.doAutosuggest );
 	};
 
-	// Do we meet the requirements?
-	that.meetsRequirements = function() {
-		return that.$c.recipesPage.length;
+ 	// Cache all the things.
+	that.cache = function() {
+		that.$c = {
+			window: $(window),
+			body: $('body'),
+			wp_debug: recipes.wp_debug,
+			autosuggest: recipes.autosuggest,
+			ingredient: '.cmb-repeatable-grouping input.ingredient',
+			recipesPage: $( 'body.post-type-rb_recipe' ),
+			removeGroupRowButton: $( '.ingredients .cmb-remove-row .cmb-remove-group-row' ),
+		};
 	};
 
 	// Replace HTML with a Dashicon "trash can".
@@ -38,55 +31,34 @@ window.recipe_box_remove_group_row = {};
 		that.$c.removeGroupRowButton.html( '<span class="dashicons dashicons-trash"></span>' );
 	};
 
-	// Engage!
-	$( that.init );
+	that.doAutosuggest = function() {
 
-})( window, jQuery, window.recipe_box_remove_group_row );
-
-
-window.recipe_box_autosuggest = {};
-( function( window, $, that ) {
-
-	// Constructor.
-	that.init = function() {
-		that.cache();
-
-		if ( that.meetsRequirements ) {
-			that.bindEvents();
-		}
-	};
-
- 	// Cache all the things.
-	that.cache = function() {
-		that.$c = {
-			window: $(window),
-			wp_debug: recipes.wp_debug,
-			autosuggest: recipes.autosuggest,
-			ingredient: '.cmb-repeatable-grouping input.ingredient',
-		};
-	};
-
-	// Show console log if debugging is active.
-	if ( that.$c.wp_debug ) {
-		console.log( that.$c.autosuggest );
-	}
-
-	if ( that.$c.autosuggest ) {
-		// wp_localize_script will convert our array into an object. We need to convert it back to an array.
-		that.$c.autosuggest = $.map( that.$c.autosuggest, function( value, index ){
-			return [value];
-		});
-
-		// Log the autosuggest array after we've modified it.
+		// Show console log if debugging is active.
 		if ( that.$c.wp_debug ) {
 			console.log( that.$c.autosuggest );
 		}
 
-		// When an ingredient input is clicked, trigger the autocompletion script.
-		$('body').on('click',that.$c.ingredient,function() {
-			$(this).autocomplete({
-				source: that.$c.autosuggest,
+		if ( that.$c.autosuggest ) {
+			// wp_localize_script will convert our array into an object. We need to convert it back to an array.
+			that.$c.autosuggest = $.map( that.$c.autosuggest, function( value, index ){
+				return [value];
 			});
-		} )
-	}
-})( window, jQuery, window.recipe_box_autosuggest );
+
+			// Log the autosuggest array after we've modified it.
+			if ( that.$c.wp_debug ) {
+				console.log( that.$c.autosuggest );
+			}
+
+
+			// When an ingredient input is clicked, trigger the autocompletion script.
+			that.$c.body.on('click',that.$c.ingredient,function() {
+				$(this).autocomplete({
+					source: that.$c.autosuggest,
+				});
+			} );
+		}
+	};
+
+	// Engage!
+	$( that.init );
+})( window, jQuery, window.recipe_box );
